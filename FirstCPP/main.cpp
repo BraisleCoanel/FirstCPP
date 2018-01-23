9910cc5d95ff44deeb8cd889a7ef6129ffddeb3a
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-01-23 12:32:10 -0700

    Created FirstCPP

---
+++ FirstCPP/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  FirstCPP
+//
+//  Created by Martinson, Branton on 1/23/18.
+//  Copyright © 2018 Martinson, Branton. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!" << endl;
+    string input;
+    getline(cin, input);
+    cout << "You said: " << input << endl;
+    return 420;
+}
